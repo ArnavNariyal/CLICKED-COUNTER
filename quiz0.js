d4453ab70bdb94583ae1684e97ee09d1936dc2e6
HJHARSH05 <hj2005sj2009@gmail.com>
2023-10-29 13:40:21 +0530

    first commit

---
+++ quiz0.js
@@ -0,0 +1,39 @@
+const quizData=[{
+    Question: "father of mohan"
+    a:"java",
+    b:"java",
+    c:"java",
+    d:"all of these",
+}
+{
+Question: "father of mohan"
+a:"java",
+b:"java",
+c:"java",
+d:"all of these",
+}
+{
+Question: "father of mohan"
+a:"java",
+b:"java",
+c:"java",
+d:"all of these",
+}
+{
+Question: "father of mohan"
+a:"java",
+b:"java",
+c:"java",
+d:"all of these",
+}
+{
+Question: "father of mohan"
+a:"java",
+b:"java",
+c:"java",
+d:"all of these",
+}
+]
+let index=0;
+let correct=0;
+let incorrect=0;
